a66fdab5616af7e41bfea5b07f7ae6e967728668
Melon-ali <rubel300ali@gmail.com>
2025-04-30 17:58:18 +0600

    Formatting Response Using the sendResponse Function

--- src/app/modules/admin/admin.controller.ts
+++ src/app/modules/admin/admin.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { AdminService } from "./admin.service";
 import pick from "../../../shared/pick";
 import { adminFilterableFields } from "./admin.constant";
+import sendResponse from "../../../shared/sendResponse";
 
 const getAllFormDb = async (req: Request, res: Response) => {
   try {
@@ -10,12 +11,19 @@ const getAllFormDb = async (req: Request, res: Response) => {
     console.log(options);
     console.log("Options", options);
     const result = await AdminService.getAllFormDb(filters, options);
-    res.status(200).json({
+    // res.status(200).json({
+    //   success: true,
+    //   message: "Admin Data fetched",
+    //   meta: result.meta,
+    //   data: result.data,
+    // });
+    sendResponse(res, {
+      statusCode: 200,
       success: true,
       message: "Admin Data fetched",
       meta: result.meta,
       data: result.data,
-    });
+    })
   } catch (error) {
     res.status(500).json({
       success: false,
@@ -29,9 +37,10 @@ const getByIdFromDB = async (req: Request, res: Response) => {
   const { id } = req.params;
   try {
     const result = await AdminService.getByIdFromDB(id);
-    res.status(200).json({
+    sendResponse(res, {
+      statusCode: 200,      
       success: true,
-      message: "Admin Data fetched",
+      message: "Admin Data fetched By Id",
       data: result,
     });
   } catch (error) {
@@ -47,7 +56,8 @@ const updateIntoDB = async (req: Request, res: Response) => {
   const { id } = req.params;
   try {
     const result = await AdminService.updateIntoDB(id, req.body);
-    res.status(200).json({
+    sendResponse(res, {
+      statusCode: 200,
       success: true,
       message: "Updated Successfully",
       data: result,
@@ -65,7 +75,8 @@ const deleteFormDB = async (req: Request, res: Response) => {
   const { id } = req.params;
   try {
     const result = await AdminService.deleteFromDB(id);
-    res.status(200).json({
+    sendResponse(res, {
+      statusCode: 200,
       success: true,
       message: "Deleteted Successfully",
       data: result,
@@ -83,7 +94,8 @@ const softDeleteFormDB = async (req: Request, res: Response) => {
   const { id } = req.params;
   try {
     const result = await AdminService.softDeleteFromDB(id);
-    res.status(200).json({
+    sendResponse(res, {
+      statusCode: 200,
       success: true,
       message: "Deleteted Successfully",
       data: result,
