905d9ec2e07be4c33b3690790f9bbd537659ccfd
Melon-ali <rubel300ali@gmail.com>
2025-05-04 14:35:44 +0600

    Extracting Refresh Token from Cookies for Token Generation

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,12 @@ import cors from "cors";
 import router from "./app/routes";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 import httpStatus from "http-status";
+import cookieParser from "cookie-parser";
 
 const app: Application = express();
 
 app.use(cors());
+app.use(cookieParser());
 
 // parser
 app.use(express.json())
